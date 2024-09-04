e8f593e0d26b4d5e153f9c1f98c1a20113450d6c
billcherrette <wi03926@intranet.secura.net>
2024-09-04 16:32:16 -0500

    namespaces, static, null, garbage collection, class library, records

---
+++ BethanyPieShopHRM.TESTS/EmployeeTests.cs
@@ -0,0 +1,32 @@
+using BethanyPieShopHRM.HR;
+
+namespace BethanyPieShopHRM.TESTS
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void PerformWork_Adds_NumberOfHours()
+        {
+            //Arrange
+            Employee employee = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+            int numberOfHours = 3;
+            //Act
+            employee.PerformWork(numberOfHours);
+            //Assert
+            Assert.Equal(numberOfHours, employee.NumberOfHoursWorked);
+        }
+
+        [Fact]
+        public void PerformWork_Adds_DefaultNumberOfHours_IfNoValueSpecified()
+        {
+            //Arrange
+            Employee employee = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+            //Act
+            employee.PerformWork();
+            //Assert
+            Assert.Equal(1, employee.NumberOfHoursWorked);
+        }
+
+    }
+}
---
+++ BethanyPieShopHRM/Accounting/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.Accounting
+{
+    //public record Account
+    //{
+    //    private string accountNumber;
+
+    //    public string AccountNumber
+    //    {
+    //        get { return accountNumber; }
+    //        set
+    //        {
+    //            accountNumber = value;
+    //        }
+    //    }
+
+    //}
+
+    public record Account(string AccountNumber);
+}
